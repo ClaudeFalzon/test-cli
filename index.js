666561551b2a297f5d50adf67ff1acd636f1cb0f
ClaudeFalzon <cjfalzon@gmail.com>
2021-12-01 14:01:24 +0100

    this is intial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log(process.argv);
+
+const { exec } = require("child_process");
+
+const [message, branchname] = process.argv.slice(2);
+
+if (message && branchname) {
+  exec("git add .", (err, stour, stderr) => {
+    if (err) {
+      throw new Error("something is wrong");
+    }
+    exec(`git commit -m "${message}"`, (err, stour, stderr) => {
+      if (err) {
+        throw new Error("something is wrong");
+      }
+
+      exec(`git push -u origin ${branchname}`, (err, stour, stderr) => {
+        if (err) {
+          throw new Error("something is wrong");
+        }
+      });
+    });
+  });
+} else {
+  console.log(
+    " please provide 2 arguments. 1st argument is the mssage you want to commit and 2nd argument is the branch name"
+  );
+}
+
+
+/* initliase project first  */
