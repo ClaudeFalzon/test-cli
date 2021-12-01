502205627208926bf5293606ba88176bc0555699
ClaudeFalzon <cjfalzon@gmail.com>
2021-12-01 14:21:40 +0100

    new test

--- index.js
+++ index.js
@@ -25,9 +25,26 @@ if (message && branchname) {
   });
 } else {
   console.log(
-    " please provide 2 arguments. 1st argument is the mssage you want to commit and 2nd argument is the branch name..."
+    " please provide 2 arguments. 1st argument is the mssage you want to commit and 2nd argument is the branch gert name..."
   );
 }
 
 
 /* initliase project first  */
+
+
+/* let { err, stdout, stderr } = execSync("git add .");
+if (!err) {
+  let { error, stdout, stderr } = execSync(`git commit -m "${message}"`);
+
+  if (!error) {
+    let { errormessage, stdout, stderr } = execSync(
+      `git push origin "${branchname}"`
+    );
+    if (!errormessage) {
+      console.log("successfully pushed your code to github");
+      console.log(stdout);
+    }
+  }
+}
+ */
